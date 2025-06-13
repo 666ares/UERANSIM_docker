812313b8c7bf477e3df5135599dfd7c5ec004747
José A. Pastor <japv@um.es>
2025-06-13 12:19:49 +0200

    refactor: remove unused configuration

--- config/ueransim_init.sh
+++ config/ueransim_init.sh
@@ -13,7 +13,6 @@ sed -i -e "s/MNC/${MNC}/g" /UERANSIM/config/gnb.yaml
 sed -i -e "s/GNB_N2_IP/${IP_ADDR}/g" /UERANSIM/config/gnb.yaml
 sed -i -e "s/GNB_N3_IP/${IP_ADDR}/g" /UERANSIM/config/gnb.yaml
 sed -i -e "s/NSSAI_SST/${NSSAI_SST}/g" /UERANSIM/config/gnb.yaml
-sed -i -e "s/NSSAI_SD/${NSSAI_SD}/g" /UERANSIM/config/gnb.yaml
 sed -i -e "s/GNB_AMF_IP/${GNB_AMF_IP}/g" /UERANSIM/config/gnb.yaml
 sed -i -e "s/TAC/${TAC}/g" /UERANSIM/config/gnb.yaml
 
@@ -21,12 +20,8 @@ sed -i -e "s/MCC/${MCC}/g" /UERANSIM/config/ue.yaml
 sed -i -e "s/MNC/${MNC}/g" /UERANSIM/config/ue.yaml
 sed -i -e "s/GNB_N2_IP/${IP_ADDR}/g" /UERANSIM/config/ue.yaml
 sed -i -e "s/NSSAI_SST/${NSSAI_SST}/g" /UERANSIM/config/ue.yaml
-sed -i -e "s/NSSAI_SD/${NSSAI_SD}/g" /UERANSIM/config/ue.yaml
-sed -i -e "s/UE_IMEI/${UE_IMEI}/g" /UERANSIM/config/ue.yaml
-sed -i -e "s/UE_IMSI/${UE_IMSI}/g" /UERANSIM/config/ue.yaml
 sed -i -e "s/UE_KEY/${UE_KEY}/g" /UERANSIM/config/ue.yaml
 sed -i -e "s/UE_OP/${UE_OP}/g" /UERANSIM/config/ue.yaml
-sed -i -e "s/UE_AMF_AUTH/${UE_AMF_AUTH}/g" /UERANSIM/config/ue.yaml
 sed -i -e "s/APN_NAME/${APN_NAME}/g" /UERANSIM/config/ue.yaml
 
 ./build/nr-gnb -c config/gnb.yaml
--- create_gnb_and_ue_session_for_container.sh
+++ create_gnb_and_ue_session_for_container.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+UE_IMSI=999251000050009
+
 NumberOfUEInstances=1
 UEsCreatedAtTheSameTime=2
-UE_IMSI=999251000050009
 ContainerName=ueransim_docker-ueransim-
-
 NumberOfGNBInstances=$(docker ps --format '{{.Names}}' | grep -c "^${ContainerName}")
 
 for (( i=1; i <= NumberOfGNBInstances; i++ ));
 do
-        echo "<@ Starting $NumberOfUEInstances UE instances (with $UEsCreatedAtTheSameTime individual UEs each) on '$ContainerName$i'"
-   for(( j=0; j < NumberOfUEInstances; j++ ))
-   do
+    echo "<@ Starting $NumberOfUEInstances UE instances (with $UEsCreatedAtTheSameTime individual UEs each) on '$ContainerName$i'"
+    for (( j=0; j < NumberOfUEInstances; j++ ))
+    do
         docker exec $ContainerName$i ./build/nr-ue -c config/ue.yaml -i $UE_IMSI -n $UEsCreatedAtTheSameTime &
         UE_IMSI=$((UE_IMSI+UEsCreatedAtTheSameTime))
         sleep 2
